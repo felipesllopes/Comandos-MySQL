84fd888568247b733a0cea16f512195acedeb73d
Felipe Silva <99768939+felipesllopes@users.noreply.github.com>
2022-10-25 20:44:42 -0300

    Adicionando arquivos

---
+++ Times-BrasileiraoAeB/INSERT-TABELA1.sql
@@ -0,0 +1,25 @@
+USE FUTEBOL;
+
+INSERT INTO BRASILEIRAO1 (NOME, ESTADO, ESTADIO, IDADE, TI_NACIONAIS, TI_INTERNAC) VALUES 
+('América-MG', 'MG', 'Arena Independência', 110, 3, 0),
+('Athletico-PR', 'PR', 'Arena da Baixada', 98, 3, 3),
+('Atlético-GO', 'GO', 'Antônio Accioly', 85, 3, 0),
+('Atlético-MG', 'MG', 'Arena Independência', 114, 7, 4),
+('Avaí', 'SC', 'Ressacada', 99, 1, 0),
+('Botafogo', 'RJ', 'Engenhão', 118, 4, 1),
+('Bragantino', 'SP', 'Nabi Abi Chedid', 94, 3, 0),
+('Ceará', 'CE', 'Castelão', 108, 0, 0),
+('Corinthians', 'SP', 'Neo Química Arena', 112, 12, 4),
+('Coritiba', 'PR', 'Couto Pereira', 103, 3, 0),
+('Cuiabá', 'MT', 'Arena Pantanal', 11, 0, 0),
+('Flamengo', 'RJ', 'Maracanã', 127, 15, 6),
+('Fluminense', 'RJ', 'Maracanã', 110, 6, 1),
+('Fortaleza', 'CE', 'Castelão', 104, 1, 0),
+('Goiás', 'GO', 'Serra Dourada', 79, 2, 0),
+('Juventude', 'RS', 'Alfredo Jaconi', 109, 2, 0),
+('Internacional', 'RS', 'Beira-Rio', 113, 5, 7),
+('Palmeiras', 'SP', 'Allianz Parque', 108, 17, 7),
+('Santos', 'SP', 'Vila Belmiro', 110, 9, 8),
+('São Paulo', 'SP', 'Morumbi', 92, 6, 12);
+
+select * from brasileirao1;
---
+++ Times-BrasileiraoAeB/INSERT-TABELA2.sql
@@ -0,0 +1,25 @@
+USE FUTEBOL;
+
+INSERT INTO BRASILEIRAO2 (NOME, ESTADO, ESTADIO, IDADE, TI_NACIONAIS, TI_INTERNAC) VALUES 
+('Bahia', 'BA', 'Arena Fonte Nova', 91, 2, 0),
+('Brusque', 'SC', 'Augusto Bauer', 35, 1, 0),
+('Chapecoense', 'SC', 'Arena Condá', 49, 1, 1),
+('CRB', 'AL', 'Rei Pelé', 110, 0, 0),
+('Criciúma', 'SC', 'Heriberto Hülse', 75, 3, 0),
+('Cruzeiro', 'MG', 'Mineirão', 101, 11, 7),
+('CSA', 'AL', 'Rei Pelé', 109, 1, 0),
+('Guarani', 'SP', 'Brinco de Ouro', 111, 2, 0),
+('Grêmio', 'RS', 'Arena do Grêmio', 119, 9, 6),
+('Ituano', 'SP', 'Novelli Júnior', 75, 2, 0),
+('Londrina', 'PR', 'Estádio do Café', 66, 1, 0),
+('Náutico', 'PE', 'Aflitos', 121, 1, 0),
+('Novorizontino', 'SP', 'Jorge Ismael', 12, 0, 0),
+('Operário', 'PR', 'Germano Krüger', 110, 2, 0),
+('Ponte Preta', 'SP', 'Moisés Lucarelli', 122, 0, 0),
+('Sampaio Corrêa', 'MA', 'Castelão', 99, 3, 0),
+('Sport', 'PE', 'Ilha do Retiro', 117, 3, 0),
+('Tombense', 'MG', 'Antônio Guimarães', 108, 1, 0),
+('Vasco', 'RJ', 'São Januário', 124, 6, 4),
+('Vila Nova', 'GO', 'Serra Dourada', 79, 1, 0);
+
+select * from brasileirao2;
